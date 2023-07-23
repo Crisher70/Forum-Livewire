54a3c209f4124bf23c26492b62854aa32efc4bf2
Adams <adamsh33@gmail.com>
2023-07-23 18:58:34 -0400

    Filtrado por titulo y por cateogira. Migracuib seeder factory de las replicas de las preguntas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,12 +12,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory()->create(['email' => 'adams@adams.cl']);
+        \App\Models\User::factory()->create(['email' => 'adams@adams.cl', 'password' => bcrypt('12345678')]);
         \App\Models\User::factory(9)->create();
         
         \App\Models\Category::factory(10)
         ->hasThreads(20)
         ->create();
+
+        \App\Models\User::factory(400)->create();
         
     }
 }
--- resources/views/livewire/show-threads.blade.php
+++ resources/views/livewire/show-threads.blade.php
@@ -1,12 +1,12 @@
 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex gap-10 py-12">
     <div class="w-64">
-        <a href="" class="block w-full py-4 mb-10 bg-gradient-to-r from-blue-600 to-blue-700 hover:to-blue-600 text-white/90 font-bold text-xs text-center rounded-md">Preguntar</a>
+        <a href="#" class="block w-full py-4 mb-10 bg-gradient-to-r from-blue-600 to-blue-700 hover:to-blue-600 text-white/90 font-bold text-xs text-center rounded-md">Preguntar</a>
 
         <ul>
             @foreach ($categories as $category)
             <li class="mb-2">
                 {{-- slate : es color de pizarra --}}
-                <a href="" class="p-2 rounded-md flex bg-slate-800 item-center gap-2 text-white/60 hover:text-white font-semibold text-xs capitalize">
+                <a href="#" wire:click.prevent="filterByCategory('{{$category->id}}')" class="p-2 rounded-md flex bg-slate-800 item-center gap-2 text-white/60 hover:text-white font-semibold text-xs capitalize">
                     <span class="w-2 h-2 rounded-full" style="background-color: {{$category->color}}"></span>
                     {{ $category->name }}
                 </a>
@@ -14,7 +14,7 @@
             @endforeach
             <li>
                 {{-- slate : es color de pizarra --}}
-                <a href="" class="p-2 rounded-md flex bg-slate-800 item-center gap-2 text-white/60 hover:text-white font-semibold text-xs capitalize">
+                <a href="#" wire:click.prevent="filterByCategory('')" class="p-2 rounded-md flex bg-slate-800 item-center gap-2 text-white/60 hover:text-white font-semibold text-xs capitalize">
                     <span class="w-2 h-2 rounded-full" style="background-color: #000"></span>
                     Todos los resultados
                 </a>
@@ -22,10 +22,21 @@
         </ul>
     </div>
     <div class="w-full">
+        <form class="mb-4">
+            <input 
+                type="text" 
+                placeholder="// ..."
+                class="bg-slate-800 border-0 rounded-md w-1/3 p-3 text-white/60 text-xs"
+                wire:model="search"
+            >
+        </form>
+
         @foreach ($threads as $thread)
             <div class="rounded-md bg-grandient-to-r from-slate-800 to-slate-900 hover:to-slate-800 mb-4">
                 <div class="p-4 flex gap-4">
-                    <div> Image, Avatar</div>
+                    <div>
+                        <img src="{{ $thread->user->avatar() }}" alt="{{ $thread->user->name }}">
+                    </div>
                     <div class="w-full"> 
                         <h2 class="mb-4 flex items-start justify-between">
                             <a href="" class="text-xl font-semibold text-white/90">
@@ -33,17 +44,26 @@
                             </a>
                             <span 
                                 class="rounded-full text-xs py-2 px-4 capitalize" 
-                                style="color: #00aced; border:1px solid #00aced;"
+                                style="color:  {{ $thread->category->color}}; border:1px solid  {{ $thread->category->color}};"
                             >
-                                Categoría
+                                {{ $thread->category->name}}
                             </span>
                         </h2>
                         <p class="flex items-center justify-between w-full text-xs">
                             <span class="text-blue-600 font-semibold">
-                                Usuario
+                                {{ $thread->user->name }}
                                 <span class="text-white/90">{{ $thread->created_at->diffForHumans() }}</span>
                             </span>
-                            <span class="text-slate-700">Respuestas y boton</span>
+                            <span class="flex items-center gap-1 text-slate-700">
+                                <svg fill="currentColor" class="h-4" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                    <path clip-rule="evenodd" fill-rule="evenodd" d="M4.804 21.644A6.707 6.707 0 006 21.75a6.721 6.721 0 003.583-1.029c.774.182 1.584.279 2.417.279 5.322 0 9.75-3.97 9.75-9 0-5.03-4.428-9-9.75-9s-9.75 3.97-9.75 9c0 2.409 1.025 4.587 2.674 6.192.232.226.277.428.254.543a3.73 3.73 0 01-.814 1.686.75.75 0 00.44 1.223zM8.25 10.875a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25zM10.875 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875-1.125a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z"></path>
+                                </svg>
+                                {{ $thread->replies_count }}
+                                Respuesta{{ $thread->replies_count != 1 ?  's' : ''}} 
+                                
+                                |
+                                <a href="" class="hover:text-white">Editar</a>
+                            </span>
                         </p>
                     </div>
                 </div>
