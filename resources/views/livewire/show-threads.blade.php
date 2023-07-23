bd7e2bbe8b9065638dc1784d857afa09148e115d
Adams <adamsh33@gmail.com>
2023-07-23 01:14:41 -0400

    Visualizacion de los hilos/comentarios de los usuarios

--- resources/views/livewire/show-threads.blade.php
+++ resources/views/livewire/show-threads.blade.php
@@ -12,13 +12,6 @@
                 </a>
             </li>
             @endforeach
-            <li class="mb-2">
-                {{-- slate : es color de pizarra --}}
-                <a href="" class="p-2 rounded-md flex bg-slate-800 item-center gap-2 text-white/60 hover:text-white font-semibold text-xs capitalize">
-                    <span class="w-2 h-2 rounded-full" style="background-color: #00aced"></span>
-                    Ejemplo
-                </a>
-            </li>
             <li>
                 {{-- slate : es color de pizarra --}}
                 <a href="" class="p-2 rounded-md flex bg-slate-800 item-center gap-2 text-white/60 hover:text-white font-semibold text-xs capitalize">
@@ -29,6 +22,32 @@
         </ul>
     </div>
     <div class="w-full">
-
+        @foreach ($threads as $thread)
+            <div class="rounded-md bg-grandient-to-r from-slate-800 to-slate-900 hover:to-slate-800 mb-4">
+                <div class="p-4 flex gap-4">
+                    <div> Image, Avatar</div>
+                    <div class="w-full"> 
+                        <h2 class="mb-4 flex items-start justify-between">
+                            <a href="" class="text-xl font-semibold text-white/90">
+                                {{ $thread->title }}
+                            </a>
+                            <span 
+                                class="rounded-full text-xs py-2 px-4 capitalize" 
+                                style="color: #00aced; border:1px solid #00aced;"
+                            >
+                                Categoría
+                            </span>
+                        </h2>
+                        <p class="flex items-center justify-between w-full text-xs">
+                            <span class="text-blue-600 font-semibold">
+                                Usuario
+                                <span class="text-white/90">{{ $thread->created_at->diffForHumans() }}</span>
+                            </span>
+                            <span class="text-slate-700">Respuestas y boton</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        @endforeach
     </div>
 </div>
