9a98ed6bf3bfc5808c6e37c54dbf17726621c925
Adams <adamsh33@gmail.com>
2023-07-22 21:17:15 -0400

    Migracion factory seeder de categorias y preguntas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,9 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory()->create(['email' => 'adams@adams.cl']);
         \App\Models\User::factory(9)->create();
         
-        \App\Models\Category::factory(10)->create();
+        \App\Models\Category::factory(10)
+        ->hasThreads(20)
+        ->create();
         
     }
 }
