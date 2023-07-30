b9caab31ea2ac2e6df4475c7d77f87fcb88f3f87
Adams <adamsh33@gmail.com>
2023-07-30 17:45:42 -0400

    Segundo componente - pregunta

---
+++ app/Http/Livewire/ShowThread.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Thread;
+
+class ShowThread extends Component
+{
+    /** la busqueda de estas variables es implicita, es decir, que se hace automativamente
+     * La consulta es http://localhost:8000/thread/195. SI no colocamos el tipo en la variable
+     * es decir, si no colocamos Thread en $thread, simplemente el programa considerara que 
+     * estamos pasando el valor 195, pero si colocamos la clase, se hara una busqueda instantanea
+     * tomando 195 como el id
+     */ 
+    public Thread $thread;
+
+    public function render()
+    {
+        return view('livewire.show-thread');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,10 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\ShowThreads;
+use App\Http\Livewire\ShowThread;
 
 Route::get('/', ShowThreads::class)->middleware(['auth'])->name('dashboard');
+Route::get('/thread/{thread}', ShowThread::class)->middleware(['auth'])->name('thread');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
