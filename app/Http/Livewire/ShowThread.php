dfd0a80a0924d04854e9c0ffed8327d8e43dbff7
Adams <adamsh33@gmail.com>
2023-07-30 18:34:34 -0400

    Crear una respuesta principal

--- app/Http/Livewire/ShowThread.php
+++ app/Http/Livewire/ShowThread.php
@@ -15,6 +15,23 @@ class ShowThread extends Component
      */ 
     public Thread $thread;
 
+    public $body = '';
+
+    public function postReply()
+    {
+        //validate
+        $this->validate(['body' => 'required']);
+
+        //craate
+        auth()->user()->replies()->create([
+            'thread_id' => $this->thread->id,
+            'body' => $this->body
+        ]);
+
+        //refresh
+        $this->body = '';
+    }
+
     public function render()
     {
         return view('livewire.show-thread');
