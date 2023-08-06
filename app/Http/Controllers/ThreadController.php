e4438a8cdb8fc6d0be14a3aa1f2dc611fa8452b3
Adams <adamsh33@gmail.com>
2023-08-06 17:29:52 -0400

    Actualizar pregunta

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -13,4 +13,17 @@ class ThreadController extends Controller
         $categories = Category::get();
         return view('thread.edit', compact('categories', 'thread'));
     }
+
+    public function update(Request $request, Thread $thread)
+    {
+        $request->validate([
+            'category_id' => 'required',
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        $thread->update($request->all());
+
+        return redirect()->route('thread', $thread );
+    }
 }
--- resources/views/thread/form.blade.php
+++ resources/views/thread/form.blade.php
@@ -8,7 +8,7 @@
         @foreach ($categories as $category)
             <option 
                 value="{{ $category->id }}"
-                @if ($thread->category_id == $category->id)
+                @if (old('category_id',$thread->category_id == $category->id))
                     selected
                 @endif
             >
@@ -17,12 +17,19 @@
         @endforeach
     </select>
 
+    {{-- 
+        si la validación de los datos del formulario falla, 
+        old('body', $thread->title) rellenará el campo title con el último valor 
+        que el usuario había introducido. Si no hay ningún valor antiguo disponible 
+        (por ejemplo, si es la primera vez que el usuario ve el formulario), 
+        entonces se utilizará $thread->title.    
+    --}}
     <input 
         type="text" 
         name="title" 
         placeholder="Titulo"
         class="bg-slate-800 border-1 border-slate-900 rounded-md w-full p-3 text-white/60 text-xs mb-4"
-        value="{{ $thread->title }}"
+        value="{{ old('body', $thread->title) }}"
     >
 
     <textarea 
@@ -31,6 +38,8 @@
         placeholder="Descripción del problema"
         class="bg-slate-800 border-1 border-slate-900 rounded-md w-full p-3 text-white/60 text-xs mb-4"
     >
-    {{ $thread->body }}
+    {{-- El método old en Laravel se utiliza para recuperar un valor antiguo 
+        que se ha enviado con los datos del formulario. --}}
+    {{ old('body',  $thread->body) }}
     </textarea>
 </div>
