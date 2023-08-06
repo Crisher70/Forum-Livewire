2d55ba7fb5c76aca4717361d445adc985c0fa743
Adams <adamsh33@gmail.com>
2023-08-06 17:10:23 -0400

    Campos del formulario

--- resources/views/thread/form.blade.php
+++ resources/views/thread/form.blade.php
@@ -1,3 +1,36 @@
 <div>
+    <select 
+        name="category_id" 
+        class="bg-slate-800 border-1 border-slate-900 rounded-md w-full p-3 text-white/60 text-xs capitalize mb-4"
     
+    >
+        <option value="">Seleccionar cateoria</option>
+        @foreach ($categories as $category)
+            <option 
+                value="{{ $category->id }}"
+                @if ($thread->category_id == $category->id)
+                    selected
+                @endif
+            >
+                {{ $category->name }}
+            </option>
+        @endforeach
+    </select>
+
+    <input 
+        type="text" 
+        name="title" 
+        placeholder="Titulo"
+        class="bg-slate-800 border-1 border-slate-900 rounded-md w-full p-3 text-white/60 text-xs mb-4"
+        value="{{ $thread->title }}"
+    >
+
+    <textarea 
+        name="body" 
+        rows="10"
+        placeholder="Descripción del problema"
+        class="bg-slate-800 border-1 border-slate-900 rounded-md w-full p-3 text-white/60 text-xs mb-4"
+    >
+    {{ $thread->body }}
+    </textarea>
 </div>
