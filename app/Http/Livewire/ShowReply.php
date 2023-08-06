2de7eb3af73b88fe7be94ffee34c180543bf5099
Adams <adamsh33@gmail.com>
2023-08-06 00:49:44 -0400

    Editar respuesta

--- app/Http/Livewire/ShowReply.php
+++ app/Http/Livewire/ShowReply.php
@@ -10,16 +10,30 @@ class ShowReply extends Component
     public Reply $reply;
     public $body = '';
     public $is_creating = false;
+    public $is_editing = false;
 
     // Esto indica que el componente ShowReply está escuchando un evento llamado 'refresh'. 
     // Cuando este evento es emitido, el componente ejecutará la acción '$refresh'.
     // Si se cambia el nombre de $refresh, sigue funcionando
     public $listeners = ['refresh' => '$refresh'];
 
+    //parecido a watch en vue. Si is_editing cambia de valor, se ejecuta lo que esta en la funcion
+    // podemos pasar parametros para poder captar el cambio, es decir, se puede colocar updatedIsEditing($valor)
+    public function updatedIsEditing()
+    {
+        $this->is_creating = false;
+        $this->body = $this->reply->body;
+    }
+
+    public function updatedIsCreating()
+    {
+        $this->is_editing = false;
+        $this->body = '';
+    }
+
     public function postChild()
     {
         if(!is_null($this->reply->reply_id)) return;
-        //dd('llego', $this->reply, $this->body,$this->reply->thread->id );
         //validate
         $this->validate(['body' => 'required']);
 
@@ -34,6 +48,19 @@ class ShowReply extends Component
         $this->body = '';
         $this->is_creating = false;
         $this->emitSelf('refresh');
+    }
+
+    public function updateReply()
+    {
+        //validate
+        $this->validate(['body' => 'required']);
+
+        //update
+        $this->reply->update([
+            'body' => $this->body
+        ]);
+
+        $this->is_editing = false;
     }
 
     public function render()
