714433fa568f35f096a6b3f1c4eb62d35c6379ff
Adams <adamsh33@gmail.com>
2023-08-06 02:37:49 -0400

    Formulario de editar pregunta

--- resources/views/livewire/show-threads.blade.php
+++ resources/views/livewire/show-threads.blade.php
@@ -1,6 +1,8 @@
 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex gap-10 py-12">
     <div class="w-64">
-        <a href="#" class="block w-full py-4 mb-10 bg-gradient-to-r from-blue-600 to-blue-700 hover:to-blue-600 text-white/90 font-bold text-xs text-center rounded-md">Preguntar</a>
+        <a href="#" class="block w-full py-4 mb-10 bg-gradient-to-r from-blue-600 to-blue-700 hover:to-blue-600 text-white/90 font-bold text-xs text-center rounded-md">
+            Preguntar
+        </a>
 
         <ul>
             @foreach ($categories as $category)
@@ -62,7 +64,7 @@
                                 Respuesta{{ $thread->replies_count != 1 ?  's' : ''}} 
                                 
                                 |
-                                <a href="" class="hover:text-white">Editar</a>
+                                <a href="{{ route('threads.edit', $thread) }}" class="hover:text-white">Editar</a>
                             </span>
                         </p>
                     </div>
